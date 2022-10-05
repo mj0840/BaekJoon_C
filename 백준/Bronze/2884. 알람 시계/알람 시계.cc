67728ae9b40b4180eadc1c465db35a3f0f3a3a21
mj0840 <106064778+mj0840@users.noreply.github.com>
2022-10-05 12:44:34 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int h, m;
+	scanf("%d %d", &h, &m);
+	m -= 45;
+	if (m < 0) {
+		m += 60;
+		h -= 1;
+		if (h < 0) h += 24;
+	}
+	printf("%d %d", h, m);
+	return 0;
+}
