d49656d7294db81fdc6c7318ec4f6627703460b3
mj0840 <106064778+mj0840@users.noreply.github.com>
2022-11-28 10:54:55 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int n, num, sum = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%1d", &num);
+		sum += num;
+	}
+	printf("%d", sum);
+}
