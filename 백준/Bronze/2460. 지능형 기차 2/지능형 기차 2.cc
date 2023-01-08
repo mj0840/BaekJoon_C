fa994bf2c0c55028c502c825195f76de824a6f04
mj0840 <106064778+mj0840@users.noreply.github.com>
2023-01-09 08:15:16 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int station = 0, total = 0;
+	int get_out, get_on;
+	for (int i = 0; i < 10; i++) {
+		scanf("%d %d", &get_out, &get_on);
+		station = station - get_out + get_on;
+		if (station > total) total = station;
+	}
+	printf("%d", total);
+	return 0;
+}
