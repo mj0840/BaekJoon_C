e887039bfd5cad147ba5f5766cb0ed9568f27bd1
mj0840 <106064778+mj0840@users.noreply.github.com>
2023-03-03 07:42:59 +0900

    [Bronze III] Title: 히스토그램, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int n, k;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &k);
+		for (int j = 0; j < k; j++) {
+			printf("=");
+		}
+		printf("\n");
+	}
+	return 0;
+}
