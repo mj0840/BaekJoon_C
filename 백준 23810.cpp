ca9f5eb8fd7855160e6aaf956bd80c1b40bf19b1
mj0840 <mj.cho0328@gmail.com>
2023-03-20 07:34:42 +0900

    백준 23810

---
+++ 백준 23810.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5 * n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5 * n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < 2 * n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	return 0;
+}
