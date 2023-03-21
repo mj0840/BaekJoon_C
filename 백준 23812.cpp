62a5a82c58312a1bcccdd9231c3b9b125d562518
mj0840 <mj.cho0328@gmail.com>
2023-03-22 08:26:56 +0900

    백준 23812

---
+++ 백준 23812.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5 * n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5 * n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < n; j++) {
+			printf("@");
+		}
+		printf("\n");
+	}
+	return 0;
+}
