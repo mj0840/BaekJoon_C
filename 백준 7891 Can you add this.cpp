ed4273c3f6ade23705c98105708b1b9ef6223e5f
mj0840 <mj.cho0328@gmail.com>
2023-03-30 09:13:10 +0900

    백준 7891 Can you add this?

---
+++ 백준 7891 Can you add this.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int t, x, y;
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d", &x, &y);
+		printf("%d\n", x + y);
+	}
+	return 0;
+}
