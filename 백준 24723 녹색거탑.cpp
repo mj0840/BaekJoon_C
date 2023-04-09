689bdc33f96f59588cabf5831705cca25c46c827
mj0840 <mj.cho0328@gmail.com>
2023-04-10 08:26:26 +0900

    백준 24723 녹색거탑

---
+++ 백준 24723 녹색거탑.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int n, sum = 1;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		sum *= 2;
+	}
+	printf("%d", sum);
+	return 0;
+}
